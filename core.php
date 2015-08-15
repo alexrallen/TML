e42e4a7345d5dba644be1a2212922d39c3890020
gcleven <graham.cleven@gmail.com>
2015-08-14 22:40:16 -0400

    create core.php

---
+++ core.php
@@ -0,0 +1,27 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+echo "
+
+<form action="test.php" method="post">
+Name: <input type="text" name="name"><br>
+EPS File: <input type="file" name="design"><br>
+<input type="submit">
+</form>
+
+";
+
+?>
