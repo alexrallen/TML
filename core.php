9c659ac494d99d9e1034dffd530aa6558135f8fc
gcleven <graham.cleven@gmail.com>
2015-08-19 00:30:50 -0400

    Complete rework of web facing front
    Removed database connection code (don't need it here)
    established framework for selecting values
    
    TODO: Add some form of protection to prevent random users submitting data to form

--- core.php
+++ core.php
@@ -1,31 +1,78 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Submit Design</title>
+<style>
+label,a 
+{
+	font-family : Arial, Helvetica, sans-serif;
+	font-size : 12px; 
+}
+
+</style>	
+</head>
+
+<body>
 <?php
+	if(isset($_POST['formSubmit'])) 
+	{
+		$vertical = $_POST['vertical'];
+		$errorMessage = "";
+		
+		if(empty($vertical)) 
+		{
+			$errorMessage = "<li>You forgot to select the vertical placement!</li>";
+		}
+		
+		if($errorMessage != "") 
+		{
+			echo("<p>There was an error with your form:</p>\n");
+			echo("<ul>" . $errorMessage . "</ul>\n");
+		} 
+		else 
+		{
+		
+			switch($vertical)
+			{
+				case ".5": $vertical = ".5"; break;
+				case "1": $vertical = "1"; break;
+				case "1.5": $vertical = "1.5"; break;
+				case "2": $vertical = "2"; break;
+				case "2.5": $vertical = "2.5"; break;
+				case "3": $vertical = "3"; break;
+				case "3.5": $vertical = "3.5"; break;
+				case "4": $vertical = "4"; break;
+				case "4.5": $vertical = "4.5"; break;
+				case "5": $vertical = "5"; break;
+				case "5.5": $vertical = "5.5"; break;
+				case "6": $vertical = "6"; break;
+				default: echo("Error!"); exit(); break;
+			}
+			echo " vertical placement is: $vertical ";
+			
+			exit();
+		}
+	}
+?>
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-
-echo "
-
-<form action="test.php" method="post">
-Name: <input type="text" name="name"><br>
-EPS File: <input type="file" name="design"<br>
-Item color: <input type="text" name="color"><br>
-Enabled? (not recomended): <input type="text" name="enabled"><br>
-Distance from top: <input type="text" name="location"><br>
-Size of eps: <input type="text" name="size"><br>
-<input type="submit">
+<form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+	<label for='vertical'>Vertical Placement</label><br>
+	<select name="vertical">
+		  <option value=".5">.5 from top</option>
+                  <option value="1">1 from top</option>
+                  <option value="1.5">1.5 from top</option>
+                  <option value="2">2 from top</option>
+                  <option value="2.5">2.5 from top</option>
+                  <option value="3">3 from top</option>
+                  <option value="3.5">3.5 from top</option>
+                  <option value="4">4 from top</option>
+                  <option value="4.5">4.5 from top</option>
+                  <option value="5">5 from top</option>
+                  <option value="5.5">5.5 from top</option>
+                  <option value="6">6 from top</option>
+	</select> 
+	<input type="submit" name="formSubmit" value="Submit" />
 </form>
 
-";
-
-?>
+</body>
+</html>
