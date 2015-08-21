d08ce872a3bda0f67893cf58a7980c328da76a8e
gcleven <graham.cleven@gmail.com>
2015-08-21 15:20:28 -0400

    ink color form, write to text file
    pretty much finished with this bit

--- core.php
+++ core.php
@@ -66,7 +66,7 @@ if ( isset( $_POST[ 'data' ] ) ) {
                 exit( );
                 break;
         }
-        echo " vertical placement is: $vertical ";
+        echo " vertical placement is: $vertical <br>";
         
     }
 }
@@ -271,7 +271,7 @@ if ( isset( $_POST[ 'data' ] ) ) {
                 exit( );
                 break;
         }
-        echo " The color is: $color ";
+        echo " The color is: $color <br>";
         
     }
 }
@@ -279,7 +279,7 @@ if ( isset( $_POST[ 'data' ] ) ) {
       <?php
 if ( isset( $_POST[ 'data' ] ) ) {
     $price = $_POST[ 'price' ];
-    echo "The price is: $price bucks";
+    echo "The price is: $price bucks <br>";
 }
 ?>
 
@@ -287,7 +287,7 @@ if ( isset( $_POST[ 'data' ] ) ) {
 
 if ( isset( $_POST[ 'data' ] ) ) {
     $name = $_POST[ 'name' ];
-    echo "The product name is: $name";
+    echo "The product name is: $name<br>";
 }
 ?>
 
@@ -295,14 +295,14 @@ if ( isset( $_POST[ 'data' ] ) ) {
 
 if ( isset( $_POST[ 'data' ] ) ) {
     $description = $_POST[ 'description' ];
-    echo "The product description is: $description";
+    echo "The product description is: $description <br> ";
 }
 ?>
 
 <?php 
 if ( isset($_POST[ 'data' ] ) ) {
     $size = $_POST[ 'size' ];
-    print $size;
+    echo "The length is: $size <br> The width is: $size <br>";
 }
 ?>
 
@@ -316,6 +316,168 @@ $info = pathinfo($_FILES['userFile']['name']);
  $target = 'images/'.$newname;
  move_uploaded_file( $_FILES['image']['tmp_name'], $target);
 }
+?>
+
+<?php
+if (isset($_POST[ 'data' ]) and $_POST[ 'Black' ] == "on") {
+echo "Black ON<br>";
+$black = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Grey' ] == "on") {
+echo "Grey ON<br>";
+$grey = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Light_Grey' ] == "on" ) {
+echo "Light Grey ON<br>";
+$lightgrey = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'White' ] == "on" ) {
+echo "White ON<br>";
+$white = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Red' ] == "on" ) {
+echo "Red ON<br>";
+$red = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Orange_Red' ] == "on" ) {
+echo "Orange Red ON<br>";
+$orangered="true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Hot_Pink' ] == "on" ) {
+echo "Hot Pink ON<br>";
+$hotpink="true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Magenta' ] == "on" ) {
+echo "Magenta ON<br>";
+$magenta="true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Charity_Pink' ] == "on" ) {
+echo "Charity Pink ON<br>";
+$charitypink = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Pink' ] == "on" ) {
+echo "Pink ON<br>";
+$pink = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Tan' ] == "on" ) {
+echo "Tan ON<br>";
+$tan = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Lemon' ] == "on" ) {
+echo "Lemon ON<br>";
+$lemon = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Athletic_Gold' ] == "on" ) {
+echo "Athletic Gold ON<br>";
+$athleticgold = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Orange' ] == "on" ) {
+echo "Orange ON<br>";
+$orange = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Yellow' ] == "on" ) {
+echo "Yellow ON<br>";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Old_Gold' ] == "on" ) {
+echo "Old Gold ON<br>";
+$yellow = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Brown' ] == "on" ) {
+echo "Brown ON<br>";
+$brown = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Cardinal' ] == "on" ) {
+echo "Cardinal ON<br>";
+$cardinal = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Maroon' ] == "on" ) {
+echo "Maroon ON<br>";
+$maroon = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Kelly_Green' ] == "on" ) {
+echo "Kelly Green ON<br>";
+$kellygreen = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Lime' ] == "on" ) {
+echo "Lime ON<br>";
+$lime = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Forest' ] == "on" ) {
+echo "Forest ON<br>";
+$forest = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Green' ] == "on" ) {
+echo "Green ON<br>";
+$green = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Olive' ] == "on" ) {
+echo "Olive ON<br>";
+$olive = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Light_Blue' ] == "on" ) {
+echo "Light Blue ON<br>";
+$lightblue = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Turquoise' ] == "on" ) {
+echo "Turquoise ON<br>";
+$turquoise = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Blue' ] == "on" ) {
+echo "Blue ON<br>";
+$blue = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Royal' ] == "on" ) {
+echo "Royal ON<br>";
+$royal = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Navy' ] == "on" ) {
+echo "Navy ON<br>";
+$navy = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Light_Purple' ] == "on" ) {
+echo "Light Purple ON<br>";
+$lightpurple = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Purple' ] == "on" ) {
+echo "Purple ON<br>";
+$purple = "true";
+}
+
+if (isset($_POST[ 'data' ]) and $_POST[ 'Dark_Grey' ] == "on" ) {
+echo "Dark Grey ON<br>";
+$darkgrey = "true";
+}
+
 ?>
 
 <?php 
@@ -333,6 +495,139 @@ $description = "$description\n";
 fwrite($database, $description);
 $size = "$size\n";
 fwrite($database, $size); #length / width 
+
+if ($black == "true") {
+fwrite($database, "Black\n");
+}
+
+if ($grey == "true") {
+fwrite($database, "Grey\n");
+}
+
+if ($lightgrey == "true") {
+fwrite($database, "Light Grey\n");
+}
+
+if ($white == "true") {
+fwrite($database, "White\n");
+}
+
+if ($red == "true") {
+fwrite($database, "Red\n");
+}
+
+if ($orangered == "true") {
+fwrite($database, "Orange Red\n");
+}
+
+if ($hotpink == "true") {
+fwrite($database, "Hot Pink\n");
+}
+
+if ($grey == "true") {
+fwrite($database, "Grey\n");
+}
+
+if ($magenta = "true") {
+fwrite($database, "Magenta\n");
+}
+
+if ($charitypink == "true") {
+fwrite($database, "Charity Pink\n");
+}
+
+if ($pink == "true") {
+fwrite($database, "Pink\n");
+}
+
+if ($tan == "true") {
+fwrite($database, "Tan\n");
+}
+
+if ($lemon == "true") {
+fwrite($database, "Lemon\n");
+}
+
+if ($athleticgold == "true") {
+fwrite($database, "Athletic Gold\n");
+}
+
+if ($orange == "true") {
+fwrite($database, "Orange\n");
+}
+
+if ($yellow == "true") {
+fwrite($database, "Yellow\n");
+}
+
+if ($oldgold == "true") {
+fwrite($database, "Old Gold\n");
+}
+
+if ($brown == "true") {
+fwrite($database, "Brown\n");
+}
+
+if ($cardinal == "true") {
+fwrite($database, "Cardinal\n");
+}
+
+if ($maroon == "true") {
+fwrite($database, "Maroon\n");
+}
+
+if ($kellygreen == "true") {
+fwrite($database, "Kelly Green\n");
+}
+
+if ($lime == "true") {
+fwrite($database, "Lime\n");
+}
+
+if ($forest == "true") {
+fwrite($database, "Forest\n");
+}
+
+if ($green == "true") {
+fwrite($database, "Green\n");
+}
+
+if ($olive == "true") {
+fwrite($database, "Olive\n");
+}
+
+if ($lightblue == "true") {
+fwrite($database, "Light Blue\n");
+}
+
+if ($turquoise == "true") {
+fwrite($database, "Turquoise\n");
+}
+
+if ($blue == "true") {
+fwrite($database, "Blue\n");
+}
+
+if ($royal == "true") {
+fwrite($database, "Royal\n");
+}
+
+if ($navy == "true") {
+fwrite($database, "Navy\n");
+}
+
+if ($lightpurple == "true") {
+fwrite($database, "Light Purple\n");
+}
+
+if ($purple == "true") {
+fwrite($database, "Purple\n");
+}
+
+if ($darkgrey == "true") {
+fwrite($database, "Dark Grey\n");
+}
+
 fclose($database);
 exit();
 }
@@ -355,7 +650,7 @@ Vertical Placement:
             <option value="5">5 from top</option>
             <option value="5.5">5.5 from top</option>
             <option value="6">6 from top</option>
-         </select>
+         </select> <br> <br>
 Shirt Color:
          <select name="color">
             <option value="Antique Royal">Antique Royal</option>
@@ -419,15 +714,15 @@ Shirt Color:
  	    <option value="Texas Orange">Texas Orange</option>
  	    <option value="Vegas Gold">Vegas Gold</option>
  	    <option value="White">White</option>
-         </select>
+         </select><br> <br>
 Product Name:
-         <input type="text" name="name" />
+         <input type="text" name="name" /><br><br>
 Product Price:
-         <input type="text" name="price" />
+         <input type="text" name="price" /> <br> <br>
 Product Description: 
-	<input type="text" name="description" />
+	<input type="text" name="description" /> <br> <br>
 SVG Upload: 
-	<input name="image" type="file" />
+	<input name="image" type="file" /> <br> <br>
 Size (Length / Width): 
         <select name="size" />
 	<option value="3">3</option>
@@ -447,7 +742,41 @@ Size (Length / Width):
 	<option value="10">10</option>
 	<option value="10.5">10.5</option>
 	<option value="11">11</option>
-	<option value="11.5">11.5</option>
+	<option value="11.5">11.5</option> 
+	</select><br> <br>
+Ink Colors (MAX IS 8): <br>
+<input type="radio" id="a" name="Black" /> Black <input id="Remove" type="button" value="Remove" onclick="document.getElementById('a').checked = false;" /><br>
+<input type="radio" id="b" name="Grey" /> Grey <input id="Remove" type="button" value="Remove" onclick="document.getElementById('b').checked = false;" /><br>
+<input type="radio" id="c" name="Light_Grey" /> Light Grey <input id="Remove" type="button" value="Remove" onclick="document.getElementById('c').checked = false;" /><br>
+<input type="radio" id="d" name="White" /> White <input id="Remove" type="button" value="Remove" onclick="document.getElementById('d').checked = false;" /><br>
+<input type="radio" id="e" name="Red" /> Red <input id="Remove" type="button" value="Remove" onclick="document.getElementById('e').checked = false;" /><br>
+<input type="radio" id="f" name="Orange_Red" /> Orange Red <input id="Remove" type="button" value="Remove" onclick="document.getElementById('f').checked = false;" /><br>
+<input type="radio" id="g" name="Hot_Pink" /> Hot Pink <input id="Remove" type="button" value="Remove" onclick="document.getElementById('g').checked = false;" /><br>
+<input type="radio" id="h" name="Magenta" /> Magenta <input id="Remove" type="button" value="Remove" onclick="document.getElementById('h').checked = false;" /><br>
+<input type="radio" id="i" name="Charity_Pink" /> Charity Pink <input id="Remove" type="button" value="Remove" onclick="document.getElementById('i').checked = false;" /><br>
+<input type="radio" id="j" name="Pink" /> Pink <input id="Remove" type="button" value="Remove" onclick="document.getElementById('j').checked = false;" /><br>
+<input type="radio" id="k" name="Tan" /> Tan <input id="Remove" type="button" value="Remove" onclick="document.getElementById('k').checked = false;" /><br>
+<input type="radio" id="l" name="Lemon" /> Lemon <input id="Remove" type="button" value="Remove" onclick="document.getElementById('l').checked = false;" /><br>
+<input type="radio" id="m" name="Athletic_Gold" /> Athletic Gold <input id="Remove" type="button" value="Remove" onclick="document.getElementById('m').checked = false;" /><br>
+<input type="radio" id="n" name="Orange" /> Orange <input id="Remove" type="button" value="Remove" onclick="document.getElementById('n').checked = false;" /><br>
+<input type="radio" id="o" name="Yellow" /> Yellow <input id="Remove" type="button" value="Remove" onclick="document.getElementById('o').checked = false;" /><br>
+<input type="radio" id="p" name="Old_Gold" /> Old Gold <input id="Remove" type="button" value="Remove" onclick="document.getElementById('p').checked = false;" /><br>
+<input type="radio" id="q" name="Brown" /> Brown <input id="Remove" type="button" value="Remove" onclick="document.getElementById('q').checked = false;" /><br>
+<input type="radio" id="r" name="Cardinal" /> Cardinal <input id="Remove" type="button" value="Remove" onclick="document.getElementById('r').checked = false;" /><br>
+<input type="radio" id="s" name="Maroon" /> Maroon <input id="Remove" type="button" value="Remove" onclick="document.getElementById('s').checked = false;" /><br>
+<input type="radio" id="t" name="Kelly_Green" /> Kelly Green <input id="Remove" type="button" value="Remove" onclick="document.getElementById('t').checked = false;" /><br>
+<input type="radio" id="u" name="Lime" /> Lime <input id="Remove" type="button" value="Remove" onclick="document.getElementById('u').checked = false;" /><br>
+<input type="radio" id="v" name="Forest" /> Forest <input id="Remove" type="button" value="Remove" onclick="document.getElementById('v').checked = false;" /><br>
+<input type="radio" id="w" name="Green" /> Green <input id="Remove" type="button" value="Remove" onclick="document.getElementById('w').checked = false;" /><br>
+<input type="radio" id="x" name="Olive" /> Olive <input id="Remove" type="button" value="Remove" onclick="document.getElementById('x').checked = false;" /><br>
+<input type="radio" id="y" name="Light_Blue" /> Light Blue <input id="Remove" type="button" value="Remove" onclick="document.getElementById('y').checked = false;" /><br>
+<input type="radio" id="z" name="Turquoise" /> Turquoise <input id="Remove" type="button" value="Remove" onclick="document.getElementById('z').checked = false;" /><br>
+<input type="radio" id="aa" name="Blue" /> Blue <input id="Remove" type="button" value="Remove" onclick="document.getElementById('aa').checked = false;" /><br>
+<input type="radio" id="bb" name="Royal" /> Royal <input id="Remove" type="button" value="Remove" onclick="document.getElementById('bb').checked = false;" /><br>
+<input type="radio" id="cc" name="Navy" /> Navy <input id="Remove" type="button" value="Remove" onclick="document.getElementById('cc').checked = false;" /><br>
+<input type="radio" id="dd" name="Light_Purple" /> Light Purple <input id="Remove" type="button" value="Remove" onclick="document.getElementById('dd').checked = false;" /><br>
+<input type="radio" id="ee" name="Purple" /> Purple <input id="Remove" type="button" value="Remove" onclick="document.getElementById('ee').checked = false;" /><br>
+<input type="radio" id="ff" name="Dark_Grey" /> Dark Grey <input id="Remove" type="button" value="Remove" onclick="document.getElementById('ff').checked = false;" /><br> <br>
          <input type="submit" name="data" value="Submit" />
       </form>
    </body>
