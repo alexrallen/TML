fa4f56f2bdfcd398a5e4a252f58719faeb954e08
gcleven <graham.cleven@gmail.com>
2015-09-04 10:41:03 -0400

    Minor Formatting Updates

--- data_injection.py
+++ data_injection.py
@@ -1,21 +1,21 @@
 import MySQLdb
 
-db = MySQLdb.connect("localhost","root","","opencart" )
+db = MySQLdb.connect("localhost","root","", "opencart" )
 
 #need to define these variables....
 stype = "gildan-ultra-cotton-t-shirt"
 color = "Black"
 desid = "55a03015c4bdad6330c430c1"
 
-with open('data.txt') as f:
-    dat = []
-    while True:
-        line = list(islice(f, 10)) 
-        if line:                     
-            lines.append(line)  
-        else:
-            break
-    print lines    
+#with open('data.txt') as f:
+ #   dat = []
+  #  while True:
+   #     line = list(islice(f, 10)) 
+    #    if line:                     
+     #       lines.append(line)  
+      #  else:
+       #     break
+   # print lines    
 
 NULL = "" #shouldn't this be obvious? 
 
@@ -46,15 +46,45 @@ price = str(price)
 cursor = db.cursor()
 
 oc_product = ("INSERT INTO oc_product "
-                 "(product_id, model, sku, upc, ean, jan, isbn, mpn, location, quantity, stock_status_id, image, manufacturer_id, shipping, price, points, tax_class_id, date_available, weight, weight_class_id, length, width, height, length_class_id, subtract, minimum, sort_order, status, viewed, date_added, date_modified) "
+"(product_id, model, \
+sku, upc, \
+ean, jan, \
+isbn, mpn, \
+location, quantity, \
+stock_status_id, image, \
+manufacturer_id, shipping, \
+price, points, \
+tax_class_id, date_available, \
+weight, weight_class_id, \
+length, width, \
+height, length_class_id, \
+subtract, minimum, \
+sort_order, status, \
+viewed, date_added, \
+date_modified)"
+
                  "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
 
 oc_product_description = ("INSERT INTO oc_product_desription "
                              "() "
                              "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
 
-	#			   1   2   3        4       5       6       7       8        9     10      11    12      13  14    15      16   17      18            19      20       21                  22             23     24     25       26      27       28     29      30      31
-data_oc_product = (id, 1, (NULL), (NULL), (NULL), (NULL), (NULL), (NULL), (NULL), (1000), (5), (image), (0), (1), (price), (0), (0), ("2015-06-26"), (NULL), (NULL), (0.00000000), (0.00000000), (0.00000000), (NULL), (NULL), (NULL), (NULL), (NULL), (NULL), (NULL), (NULL)) 
+data_oc_product = (id, 1, \
+(NULL), (NULL),\
+(NULL), (NULL),\
+(NULL), (NULL), \
+(NULL), (1000), \
+(5), (image),\
+(0), (1),\
+(price), (0), \
+(0), ("2015-06-26"), \
+(NULL), (NULL), \
+(0.00000000), (0.00000000), \
+(0.00000000), (NULL),\
+(NULL), (NULL),\
+(NULL), (NULL),\
+(NULL), (NULL),\
+(NULL))
 
 cursor.execute(oc_product, data_oc_product)
 
@@ -73,6 +103,5 @@ cursor.execute(oc_attrb, data_oc_attrb_shirt)
 cursor.execute(oc_attrb, data_oc_attrb_design)
 
 #Add Sizes
-
 
 
